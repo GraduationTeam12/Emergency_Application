d8dd721ffe1ca51ac4ca27372560bbb7c0a98d4a
THE LAPTOP SHOP <doaaabdallahelsayed1@gmail.com>
2024-10-15 15:51:58 +0300

    splash_screen responsive ios / android

--- lib/constants/pages_name.dart
+++ lib/constants/pages_name.dart
@@ -2,3 +2,6 @@ const selectingMethodScreen = '/selecting-screen';
 const onBoardingScreen = '/onboarding-screen';
 const signInScreen = '/signin-screen';
 const forgotPasswordScreen = '/forgotpassword-screen';
+const splashScreen = '/splash-screen';
+
+
--- lib/presentation/screens/on_boarding.dart
+++ lib/presentation/screens/on_boarding.dart
@@ -4,31 +4,31 @@ import 'package:user_accident/presentation/widgets/on_boarding_section.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
-
   
-  @override
+  
+    @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
           backgroundColor: Colors.white,
-          toolbarHeight: MediaQuery.sizeOf(context).height / 8,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Skip",
-                    style: AppStyle.styleBold16(context),
-                  )),
-            )
-          ],
-        ),
-        body: const OnBoardingSection()
-      ),
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            toolbarHeight: MediaQuery.sizeOf(context).height / 8,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Skip",
+                      style: AppStyle.styleBold16(context),
+                    )),
+              )
+            ],
+          ),
+          body: const OnBoardingSection()
+          ),
     );
   }
 }
