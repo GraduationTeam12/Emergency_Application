92526c867c4b447c99799dbae95285262b3451e1
HossamAhmadMohamed <ugs.141252@ci.suez.edu.eg>
2024-10-15 20:43:03 +0300

    merged

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       onGenerateRoute: appRouter.generationRoute,
-      initialRoute: selectingMethodScreen ,
+      initialRoute: splashScreen ,
     );
   }
 }
